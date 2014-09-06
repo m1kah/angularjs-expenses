cf8cb9bcae42f09ec783c019eada0ff289398ac6
Mika Hamalainen <mika.hamalainen@gmail.com>
2014-09-06 08:29:54 +0300

    d3 js example

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,11 +7,10 @@ expensesApp.controller('ExpensesController', function($scope, $http) {
   });
   
   $scope.predicate = 'receiver';
-  $scope.from_date = '2014-06-01'; // moment.startOf('month');
-  $scope.to_date = '2014-06-30'; // moment.endOf('month');
+  $scope.from_date = '2014-08-01'; // moment.startOf('month');
+  $scope.to_date = '2014-08-30'; // moment.endOf('month');
   
   $scope.search = function() {
-    $scope.from_date = 'ok';
     $http.get('api/transactions', {
       params: {
         from_date: moment($scope.from_date).toDate(),
@@ -19,5 +18,35 @@ expensesApp.controller('ExpensesController', function($scope, $http) {
     }}).success(function(data) {
       $scope.transactions = data;
     });
+  };
+  
+  var getCategory = function(categoryName) {
+    for (var i = 0; i < $scope.categories.length; i++) {
+      var category = $scope.categories[i];
+      console.log('category.name: ' + category.name + ', categoryName: ' + categoryName);
+      if (category.name === categoryName) {
+        return category;
+      }
+    }
+    
+    console.log('No categories, creating new', categoryName);
+    
+    var category = { name: categoryName, amount: 0.00 };
+    $scope.categories.push(category);
+    return category;
+  }
+    
+  $scope.updateCategory = function() {
+    $scope.categories = [];
+    
+    for (var i = 0; i < $scope.transactions.length; i++) {
+      var tx = $scope.transactions[i];
+      
+      if (tx.category) {
+        var category = getCategory(tx.category);
+        category.amount += tx.amount;
+        console.log('category', tx.category);
+      }
+    }
   };
 });
---
+++ public/js/graph.js
@@ -0,0 +1,65 @@
+var width = 240,
+    height = 240,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg = d3.select("#category-graph").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data.csv", function(error, data) {
+  var ageNames = d3.keys(data[0]).filter(function(key) { return key === "age"; });
+
+  data.forEach(function(d) {
+    d.population = +d.population;
+  });
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.age; });
+      
+
+  var legend = d3.select("#category-graph").append("svg")
+      .attr("class", "legend")
+      .attr("width", radius * 2)
+      .attr("height", radius * 2)
+    .selectAll("g")
+      .data(color.domain().slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
+
+});
